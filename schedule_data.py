b8872f5a204d27a12b0c09e1df8fb4582aede375
Noah Haasis <haasis_noah@yahoo.de>
2018-10-13 07:07:43 +0200

    fix bug with global vars and store summary daily

--- schedule_data.py
+++ schedule_data.py
@@ -8,7 +8,16 @@ from typing import List
 
 
 def is_friday() -> bool:
-    return datetime.today().weekday() == 4
+    return current_weekday() == 4
+
+def is_weekend() -> bool:
+    return current_weekday() in [5, 6]
+
+def current_day() -> int:
+    return datetime.today().day
+
+def current_weekday() -> int:
+    return datetime.today().weekday()
 
 def current_week() -> int:
     return datetime.today().isocalendar()[1]
@@ -41,7 +50,7 @@ def fetch_week_summary() -> str:
         return response.text
     return None
 
-def fetch_all_tables() -> List[str]:
+def fetch_tables() -> List[str]:
     tables = []
     for i in count(1):
         table = fetch_table(i)
@@ -54,38 +63,44 @@ def filename_for_class(class_index: int) -> str:
     return '{}_{}__{}.html'.format(current_year(), current_week(), class_index)
 
 def filename_for_week_summary() -> str:
-    return '{}_{}__summary.html'.format(current_year(), current_week())
+    return '{}_{}_{}__summary.html'.format(
+            current_year(), current_day(), current_week())
 
 def store_table_in_dropbox(dbx, table: str, filename: str) -> None:
     dbx.files_upload(table.encode(), '/' + filename)
 
 last_stored_day = None
 def store_tables(tables: List[str]) -> None:
+    global last_stored_day
     dbx = dropbox.Dropbox(environ['dropbox_access_token'])
     for i, table in enumerate(tables):
         filename = filename_for_class(i+1)
         store_table_in_dropbox(dbx, table, filename)
-    last_stored_day = datetime.today().day
+    last_stored_day = current_day()
 
+last_stored_summary_day = None
 def store_week_summary(summary: str):
+    global last_stored_summary_day
     dbx = dropbox.Dropbox(environ['dropbox_access_token'])
     dbx.files_upload(summary.encode(), '/' + filename_for_week_summary())
+    last_stored_summary_day = current_day()
 
-def already_stored_today() -> bool:
-    return last_stored_day is not None and last_stored_day == datetime.today().day
+def already_stored_tables_today() -> bool:
+    return last_stored_day is not None and last_stored_day == current_day()
+
+def already_stored_summary_today() -> bool:
+    stored_today = last_stored_summary_day == current_day()
+    return last_stored_summary_day is not None and stored_today
 
 def main() -> None:
     HOUR = 60 * 60
-    DAY = HOUR * 24
 
     while True:
-        if is_friday() and not already_stored_today():
-            tables = fetch_all_tables()
-            store_tables(tables)
+        if is_friday() and not already_stored_tables_today():
+            store_tables(fetch_tables())
 
-            summary = fetch_week_summary()
-            store_week_summary(summary)
-            sleep(DAY * 4)
+        if not (is_weekend() or already_stored_summary_today()):
+            store_week_summary(fetch_week_summary())
 
         sleep(HOUR * 5)
 
